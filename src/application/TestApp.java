6909ff25ece9a873f77ef42b8796d1836afcc172
DominicBB <41931594+DominicBB@users.noreply.github.com>
2018-12-26 21:09:02 +1300

    Update TestApp.java

--- src/application/TestApp.java
+++ src/application/TestApp.java
@@ -1,14 +1,7 @@
 package application;
 
-import Rendering.renderingSystems.RenderSystem;
-import components.*;
-import core.Window;
 import core.coreSystems.ECSSystem;
 import systems.*;
-
-
-import java.io.IOException;
-import java.util.Arrays;
 
 public class TestApp extends ECSSystem {
     @Override

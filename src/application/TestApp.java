0dc6d64903af2858c99c0e2b7cadbcadd9e4e3d3
DominicBB <41931594+DominicBB@users.noreply.github.com>
2018-12-31 17:45:53 +1300

    updating how meshes created

--- src/application/TestApp.java
+++ src/application/TestApp.java
@@ -1,7 +1,9 @@
 package application;
 
+import Rendering.renderUtil.RenderState;
 import core.coreSystems.ECSSystem;
-import systems.*;
+import systems.MonkeyHeadSystem;
+import systems.PlayerSystem;
 
 public class TestApp extends ECSSystem {
     @Override
@@ -13,13 +15,13 @@ public class TestApp extends ECSSystem {
         super();
         createSystems();
         start();
+        RenderState.lightingState.ambientColor.add(0.5f, .5f, .5f, 0f);
     }
 
     private void createSystems() {
         new PlayerSystem();
         new MonkeyHeadSystem();
     }
-
 
 
 }
